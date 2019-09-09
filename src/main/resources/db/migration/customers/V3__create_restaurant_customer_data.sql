9a438afb37a88eb9b34dd9a2fc52575ccad8455f
Jhonny Munoz <jhonnymunoz@gmail.com>
2019-09-08 21:19:19 -0500

    added message order feature

---
+++ src/main/resources/db/migration/customers/V3__create_restaurant_customer_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO `customers`
+(`id`,
+`name`,
+`last_name`,
+`email`,
+`phone`,
+`status`)
+VALUES
+('4a135a36-cf5c-4041-bc9c-6fd2e352d6b6',
+'Restaurant',
+'On site',
+'orders@restaurant.com',
+'0000000000',
+'ACTIVE');
